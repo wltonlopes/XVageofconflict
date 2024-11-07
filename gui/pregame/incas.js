019731564c4454c3301944f6eca97d51ab7bd799
weltonlopesmetal <weltonlopesmetal@gmail.com>
2024-11-07 08:22:32 -0300

    Civ mali e jogavel

---
+++ gui/pregame/incas.js
@@ -0,0 +1,18 @@
+g_BackgroundLayerData.push(
+	[
+		{
+			"offset": (time, width) => 0.00 * width * Math.cos(0.0 * time),
+			"sprite": "background-mayas_01",
+			"tiling": false
+		},
+		{
+			"offset": (time, width) => 0.00 * width * Math.cos(0.0 * time),
+			"sprite": "background-mayas_02",
+			"tiling": false
+		},
+		{
+			"offset": (time, width) => 0.05 * width * Math.cos(0.1 * time),
+			"sprite": "background-mayas_03",
+			"tiling": false
+		}
+	]);
